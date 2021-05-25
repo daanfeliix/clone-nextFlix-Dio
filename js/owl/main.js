1e24b1d136579bcdded588902d4da54cbb207dc0
unknown <danielfelix.c@gmail.com>
2021-05-24 22:22:12 -0300

    initial project

---
+++ js/owl/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        400:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
